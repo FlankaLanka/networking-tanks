a4bb2155bc632a4549d16b29d73156fcb1676ecd
frankyang233 <frankyang233@gmail.com>
2023-09-12 22:28:32 -0700

    storing:

---
+++ Assets/Scripts/RTSPlayer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private List<Unit> myUnits = new List<Unit>();
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+            return;
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId)
+            return;
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        Unit.AuthorityOnUnitSpawned += ClientHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += ClientHandleUnitDespawned;
+    }
+    public override void OnStopClient()
+    {
+        Unit.AuthorityOnUnitSpawned -= ClientHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= ClientHandleUnitDespawned;
+    }
+
+    private void ClientHandleUnitSpawned(Unit unit)
+    {
+        if (!isOwned)
+            return;
+        myUnits.Add(unit);
+    }
+
+    private void ClientHandleUnitDespawned(Unit unit)
+    {
+        if (!isOwned)
+            return;
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
 
 public class Unit : NetworkBehaviour
 {
-
     [SerializeField] private UnityEvent onSelected;
     [SerializeField] private UnityEvent onDeselected;
+
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
 
     [SerializeField] private UnitMovement unitMovement;
     public UnitMovement GetUnitMovement()
@@ -16,7 +22,35 @@ public class Unit : NetworkBehaviour
         return unitMovement;
     }
 
+    #region Server
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    #endregion
+
+
     #region Client
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !isOwned)
+            return;
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !isOwned)
+            return;
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
 
     [Client]
     public void Select()
